746946459945c81d2775a4bdbb7c31f6ed0b48b8
Timothy Guan-tin Chien <timdream@gmail.com>
2013-08-12 01:51:10 +0800

    Remove scores after sorting the results

--- test/unit/jszhuyin_data_pack.js
+++ test/unit/jszhuyin_data_pack.js
@@ -19,67 +19,67 @@ test('decodeString()', function() {
 module('JSZhuyinDataPack');
 
 test('construct with packed string.', function() {
-  var data = new JSZhuyinDataPack('ÛIÀ台北̦ÛI@台灣');
-  equal(data.packed, 'ÛIÀ台北̦ÛI@台灣', 'Passed!');
+  var data = new JSZhuyinDataPack('ÛIÀ台北,台灣');
+  equal(data.packed, 'ÛIÀ台北,台灣', 'Passed!');
 });
 
 test('construct with structured data.', function() {
   var data = new JSZhuyinDataPack([
     { str: '台北', score: 3.1415927410125732 },
-    { str: '台灣', score: -3.1415927410125732 }
+    { str: '台灣' }
   ]);
 
   deepEqual(data.unpacked, [
     { str: '台北', score: 3.1415927410125732 },
-    { str: '台灣', score: -3.1415927410125732 }
+    { str: '台灣' }
   ], 'Passed!');
 });
 
 test('unpack()', function() {
-  var data = new JSZhuyinDataPack('ÛIÀ台北̦ÛI@台灣');
+  var data = new JSZhuyinDataPack('ÛIÀ台北,台灣');
   data.unpack();
 
   deepEqual(data.unpacked, [
     { str: '台北', score: -3.1415927410125732 },
-    { str: '台灣', score: 3.1415927410125732 }
+    { str: '台灣' }
   ], 'Passed!');
 });
 
 test('pack()', function() {
   var data = new JSZhuyinDataPack([
     { str: '台北', score: -3.1415927410125732 },
-    { str: '台灣', score: 3.1415927410125732 }
+    { str: '台灣' }
   ]);
   data.pack();
 
-  equal(data.packed, 'ÛIÀ台北̦ÛI@台灣', 'Passed!');
+  equal(data.packed, 'ÛIÀ台北,台灣', 'Passed!');
 });
 
 test('getResults()', function() {
-  var data = new JSZhuyinDataPack('ÛIÀ台北̦ÛI@台灣');
+  var data = new JSZhuyinDataPack('ÛIÀ台北,台灣');
   deepEqual(data.getResults(), [
     { str: '台北', score: -3.1415927410125732 },
-    { str: '台灣', score: 3.1415927410125732 }
+    { str: '台灣' }
   ], 'Passed!');
 });
 
 test('getPackedString()', function() {
   var data = new JSZhuyinDataPack([
     { str: '台北', score: -3.1415927410125732 },
-    { str: '台灣', score: 3.1415927410125732 }
+    { str: '台灣' }
   ]);
-  equal(data.getPackedString(), 'ÛIÀ台北̦ÛI@台灣', 'Passed!');
+  equal(data.getPackedString(), 'ÛIÀ台北,台灣', 'Passed!');
 });
 
 test('getFirstResult()', function() {
-  var data = new JSZhuyinDataPack('ÛIÀ台北̦ÛI@台灣');
+  var data = new JSZhuyinDataPack('ÛIÀ台北,台灣');
   deepEqual(data.getFirstResult(),
     { str: '台北', score: -3.1415927410125732 },
     'Passed!');
 });
 
 test('getFirstResultScore()', function() {
-  var data = new JSZhuyinDataPack('ÛIÀ台北̦ÛI@台灣');
+  var data = new JSZhuyinDataPack('ÛIÀ台北,台灣');
   equal(data.getFirstResultScore(),
     -3.1415927410125732, 'Passed!');
 });
