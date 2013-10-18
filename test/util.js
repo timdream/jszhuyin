a3acbe9b27d44b19bf35a22e8fa1446c658fb6d6
Timothy Guan-tin Chien <timdream@gmail.com>
2013-10-18 14:38:52 +0800

    Remove IndexedDBStorage and JSONStorage, use BinStorage w/ tests

--- test/util.js
+++ test/util.js
@@ -1,6 +1,13 @@
 'use strict';
 
 var arrayBufferToArray = function arrayBufferToArray(buf) {
+  if (typeof buf === 'undefined' ||
+      buf.constructor !== ArrayBuffer)
+    return buf;
+
+  if (buf.byteLength % Uint16Array.BYTES_PER_ELEMENT)
+    throw 'byteLength (' + buf.byteLength + ') is not divisible by 4.';
+
   return Array.prototype.concat.apply([], new Uint16Array(buf))
     .map(function(n) {
       return '0x' + n.toString(16)
@@ -8,5 +15,12 @@ var arrayBufferToArray = function arrayBufferToArray(buf) {
 };
 
 var arrayBufferToString = function arrayBufferToArray(buf) {
+  if (typeof buf === 'undefined' ||
+      buf.constructor !== ArrayBuffer)
+    return buf;
+
+  if (buf.byteLength % Uint16Array.BYTES_PER_ELEMENT)
+    throw 'byteLength (' + buf.byteLength + ') is not divisible by 4.';
+
   return String.fromCharCode.apply(String, new Uint16Array(buf));
 };
