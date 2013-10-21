97fbcb0802fe008f563d806d7fdee10a2e7fa6be
Timothy Guan-tin Chien <timdream@gmail.com>
2013-10-21 15:13:34 +0800

    Don't use $TMPDIR

--- build/run-slimerjs.sh
+++ build/run-slimerjs.sh
@@ -4,10 +4,10 @@
 
 echo 'Testing '$1
 
-$SLIMERJS ./test/headless-runner.js $1 | tee ${TMPDIR}/slimerjs.log
+$SLIMERJS ./test/headless-runner.js $1 | tee ./slimerjs.log
 
 # Figure out the exit code ourselves because Gecko does not allow
 # SlimerJS to do so for now.
-[ -z "`grep '0 failed.' ${TMPDIR}/slimerjs.log`" ] && ERROR=1
-rm ${TMPDIR}/slimerjs.log
+[ -z "`grep '0 failed.' ./slimerjs.log`" ] && ERROR=1
+rm ./slimerjs.log
 exit $ERROR
