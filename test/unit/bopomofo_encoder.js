3ec8fd14b6c1b079645c53798866e736afeffd12
Timothy Guan-tin Chien <timdream@gmail.com>
2015-09-21 13:00:51 +0800

    BopomofoEncoder.getSymbolsCompositions()

--- test/unit/bopomofo_encoder.js
+++ test/unit/bopomofo_encoder.js
@@ -117,6 +117,76 @@ function() {
   equal(flag, true, 'Passed!');
 });
 
+test('encodeExpended() (completed sounds)', function() {
+  var arr =
+    BopomofoEncoder.encodeExpended('ㄓㄨˋㄧㄣˉㄕㄨˉㄖㄨˋㄈㄚˇ');
+
+  // ㄓㄨˋㄧㄣˉㄕㄨˉㄖㄨˋㄈㄚˇ
+  var expectedArr = [7940, 209, 8961, 9476, 2059];
+
+  deepEqual(arr, expectedArr, 'Passed!');
+});
+
+test('encodeExpended() (incompleted sounds)', function() {
+  var arr =
+    BopomofoEncoder.encodeExpended('ㄓㄨㄧㄣㄕㄨㄖㄨㄈㄚ');
+
+  var expectedArr = [7680, 256, 128, 80, 8704, 256, 9216, 256, 2048, 8];
+
+  deepEqual(arr, expectedArr, 'Passed!');
+});
+
+test('encodeExpended() (mix completed and incompleted sounds)',
+function() {
+  var tests = [
+    [ 'ㄓㄨㄧㄣㄕㄨˉㄖㄨㄈㄚ', [7680, 256, 128, 80, 8961, 9216, 256, 2048, 8]],
+    [ 'ㄓㄧㄕㄖㄈㄚˇ', [7680, 128, 8704, 9216, 2059]],
+    [ 'ㄓㄨˋㄧㄕㄖㄈ', [7940, 128, 8704, 9216, 2048]],
+    [ 'ˉˇ', [1, 3]]
+  ];
+
+  tests.forEach(function(test) {
+    deepEqual(BopomofoEncoder.encodeExpended(test[0]), test[1], test[0]);
+  });
+});
+
+test('getSymbolsCompositions()', function() {
+  var tests = [
+    [ 'ㄓ',
+      [ [[7680]]] ],
+    [ 'ㄓㄓ',
+      [ [[7680, 7680]],
+        [[7680], [7680]] ] ],
+    [ 'ㄓㄨ',
+      [ [[7936]],
+        [[7680], [256]] ] ],
+    [ 'ㄉㄧㄢˋ',
+      [ [[ 2764 ]]] ],
+    [ 'ㄉㄧㄢ',
+      [ [[2760]],
+        [[2560], [200]],
+        [[2688], [72]],
+        [[2560], [128], [72]] ] ],
+    ['ㄓㄨˋㄧㄣˉ',
+      [ [[7940, 209]],
+        [[7940], [209]] ] ],
+    ['ㄓㄨㄧㄣ',
+      [ [[7936, 208]],
+        [[7680], [256, 208]],
+        [[7936], [208]],
+        [[7680], [256], [208]],
+        [[7936, 128], [80]],
+        [[7680], [256, 128], [80]],
+        [[7936], [128], [80]],
+        [[7680], [256], [128], [80]] ] ]
+  ];
+
+  tests.forEach(function(test) {
+    deepEqual(
+      BopomofoEncoder.getSymbolsCompositions(test[0]), test[1], test[0]);
+  });
+});
+
 test('isIncompletionOf() should compare ㄉ with ㄉㄧㄢˋ.',
 function() {
   var flag = BopomofoEncoder.isIncompletionOf(
