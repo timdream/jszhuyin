7422fd3917190688f0254491409281651b4aba76
Timothy Guan-tin Chien <timdream@gmail.com>
2015-09-25 13:10:29 +0800

    Only output the top composed result

--- test/unit/jszhuyin.js
+++ test/unit/jszhuyin.js
@@ -177,7 +177,37 @@ test('query() a three-word phrase with completed sounds', function() {
     ime.oncandidateschange = function(results) {
       deepEqual(results,
         [['台北市', candidateId++],['臺北市', candidateId++],
-         ['台北是', candidateId++],
+         ['台北', candidateId++],
+         ['台', candidateId++],['臺', candidateId++],['抬', candidateId++],
+         ['颱', candidateId++],['檯', candidateId++],['苔', candidateId++],
+         ['跆', candidateId++],['邰', candidateId++],['鮐', candidateId++],
+         ['旲', candidateId++],['炱', candidateId++],['嬯', candidateId++],
+         ['儓', candidateId++],['薹', candidateId++],['駘', candidateId++],
+         ['籉', candidateId++],['秮', candidateId++]],
+        'Passed!');
+    };
+    ime.queue.done = function() {
+      ime.unload();
+
+      start();
+    };
+    ime.query();
+  };
+
+  stop();
+  ime.load();
+});
+
+test('query() a three-word composed result with completed sounds', function() {
+  var ime = new JSZhuyin();
+  ime.dataURL = './resources/testdata.data';
+  ime.onloadend = function() {
+    ime.symbols = 'ㄊㄞˊㄅㄟˇㄊㄞˊ';
+    expect(1);
+    var candidateId = 42;
+    ime.oncandidateschange = function(results) {
+      deepEqual(results,
+        [['台北台', candidateId++],
          ['台北', candidateId++],
          ['台', candidateId++],['臺', candidateId++],['抬', candidateId++],
          ['颱', candidateId++],['檯', candidateId++],['苔', candidateId++],
